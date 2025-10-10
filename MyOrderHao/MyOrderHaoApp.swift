9e315787f0dcfc41e508d9ca13e282bf8ca1b024
Hao Nguyen <nguyenquochao2000@gmail.com>
2025-10-10 00:40:27 -0400

    Initial Commit

---
+++ MyOrderHao/MyOrderHaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOrderHaoApp.swift
+//  MyOrderHao
+//
+//  Created by user275773 on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyOrderHaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
